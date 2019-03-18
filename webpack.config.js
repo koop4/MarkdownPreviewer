4b2d9ef1d345321310aa8ff2b8857d4af725746f
koop4 <dilillo.roberto@gmail.com>
2019-03-18 10:31:28 +0100

    fix dependencies issue

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 const htmlPlugin = new HtmlWebPackPlugin({
     template: './src/index.html',
-    filename: ".index.html"
+    filename: "./index.html"
 });
 
 module.exports = {
@@ -12,7 +12,10 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
-                    loader: "babel-loader"
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
                 }
             }
         ]
